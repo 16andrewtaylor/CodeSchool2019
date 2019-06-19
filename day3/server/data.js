20f05f631ec2c558d21f6d4c8a1657a1de910243
Andrew Taylor <16andrewtaylor@gmail.com>
2019-06-19 09:23:52 -0600

    Adding GET /names endpoint

---
+++ day3/server/data.js
@@ -0,0 +1,13 @@
+var data = {
+    names: [
+        "Phil",
+        "Larry",
+        "Bob",
+        "Carol",
+        "Bart",
+        "Curtis",
+        "Larry",
+    ],
+};
+
+module.exports = data;
--- day3/server/server.js
+++ day3/server/server.js
@@ -4,13 +4,23 @@ const cors = require( "cors" );
 var server = express( );
 var port = 8080;
 
+// Middleware
 server.use( cors( ) );
 server.use( express.json( ) );
 server.use( express.urlencoded( {
     extended: false
 } ) );
 
-//
+// Data
+var data = require( "./data.js" );
+
+// REST endpoint
+server.get( "/names", function ( req, res ) {
+    var response = {
+        names: data.names
+    };
+    res.json( response );
+});
 
 server.listen( port, function ( ) {
     console.log( `Listening on port ${ port }` );
