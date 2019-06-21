3a75811f029f2382de750241da33d89987ec77ae
Andrew Taylor <16andrewtaylor@gmail.com>
2019-06-21 12:16:54 -0600

    PUT /todos/id completed

--- day5/server/README.md
+++ day5/server/README.md
@@ -40,3 +40,31 @@ Body: { msg: "Message" }
 Status: 201  
 Content-type: none  
 Body: none  
+
+# Update: PUT /todos/id
+## - Request
+Method: PUT  
+Path: /todos/id  
+Content-type: application/json  
+Body: { name: "New todo name", completed: boolean }
+
+## - Response
+### If they didn't send over at least a name or a completed value
+Status: 400  
+Content-type: application/json  
+Body: { msg: "Message" }  
+
+### Else if they did send data to update, but the values were invalid
+Status: 400  
+Content-type: application/json  
+Body: { msg: "Message" }  
+
+### Else if there was no todo with the id given
+Status: 404  
+Content-type: application/json  
+Body: { msg: "Message" }
+
+### Ok, we updated
+Status: 204
+Content-type: none
+Body: none
--- day5/server/server.js
+++ day5/server/server.js
@@ -58,6 +58,60 @@ server.post( "/todos", function ( req, res ) {
     }
 });
 
+// PUT /todos/id
+server.put( "/todos/:id", function ( req, res ) {
+    // console.log( req.params.id );
+
+    // Check for bad request
+    var bad_request = false;
+
+    if ( req.body.name == undefined && req.body.completed == undefined ) {
+        bad_request = true;
+    }
+    if ( req.body.name != undefined && req.body.name == "" ) {
+        bad_request = true;
+    }
+    if ( req.body.completed != undefined && typeof( req.body.completed ) != "boolean" ) {
+        bad_request = true;
+    }
+
+    if ( bad_request ) {
+        var response = {
+            msg: "Please send over a name or a completed value to update, make sure they are valid as well"
+        }
+        res.status( 400 );
+        res.json( response );
+    } else {
+        // Request is good, but make sure todo with the id exists
+        var found = data.todos.some( function ( todo ) {
+            return todo.id == req.params.id;
+        });
+
+        if ( !found ) {
+            var response = {
+                msg: `We could not find a todo with the id of ${ req.params.id }`
+            }
+            res.status( 404 );
+            res.json( response );
+        } else {
+            // We have a good todo to update, now lets update it
+            data.todos.forEach( function ( todo ) {
+                if ( todo.id == req.params.id ) {
+                    if ( req.body.name != undefined ) {
+                        todo.name = req.body.name;
+                    }
+                    if ( req.body.completed != undefined ) {
+                        todo.completed = req.body.completed;
+                    }
+                }
+            });
+            res.status( 204 );
+            res.send( );
+        }
+    }
+
+});
+
 // Start the server
 server.listen( port, function ( ) {
     console.log( `Listening on port ${ port }` );
